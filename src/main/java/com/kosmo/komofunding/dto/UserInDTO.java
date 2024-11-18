11374a151a2fa467d6b812f5134eecec8ffaea0d
coding-ji <eunjikwon0530@hotmail.com>
2024-11-18 13:32:19 +0900

    초안수정

--- src/main/java/com/kosmo/komofunding/dto/UserInDTO.java
+++ src/main/java/com/kosmo/komofunding/dto/UserInDTO.java
@@ -1,38 +1,6 @@
 package com.kosmo.komofunding.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.kosmo.komofunding.common.enums.UserStatus;
-import jakarta.persistence.PrePersist;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
-
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class UserInDTO {
-    private UUID userId;
-    private Long userNumber;
-    private UserStatus activatedStatus;
-    private String deactivationReason;
-    private String email;
-    private String password;
-    private String name;
-    private String nickName;
-    private String phoneNumber;
-    private String shortDescription;
-    private String bankName;
-    private String accountNumber;
-    private String accountHolder;
-    private LocalDateTime joinDate;
-    private String corporationName;
-    private String corporationTel;
-    private Long BSN;
-    private List<UUID> projectIds; // 프로젝트 ID 목록만 포함
+
 }
--- src/main/java/com/kosmo/komofunding/repository/UserRepository.java
+++ src/main/java/com/kosmo/komofunding/repository/UserRepository.java
@@ -1,18 +1,5 @@
 package com.kosmo.komofunding.repository;
 
-import com.kosmo.komofunding.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+public interface UserRepository {
 
-import java.util.Optional;
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-    // nickname으로 사용자 찾기
-    Optional<User> findByNickName(String nickName);
-
-    // creatorId로 User를 조회하는 메서드 추가
-    Optional<User> findById(UUID userId);
-
-
-    Optional<User> findByEmailAndPassword(String email, String password);  // 사용자 인증용
 }
