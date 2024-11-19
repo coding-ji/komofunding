8341da6bdfb6f11c1d9296277014eab8eedad74b
KOSMO <chlek95@naver.com>
2024-11-19 19:13:54 +0900

    select 버튼 커밋

--- src/main/frontend/src/App.jsx
+++ src/main/frontend/src/App.jsx
@@ -1,9 +1,14 @@
-import './App.css'
+
+import SearchInput from './components/SearchInput/SearchInput';
+import SelectProjectBtn from './components/SelectProjectBtn';
+
+
 
 function App() {
   return (
     <>
-
+       <SearchInput></SearchInput>
+       <SelectProjectBtn></SelectProjectBtn>
     </>
   )
 }
--- src/main/frontend/src/components/SelectProjectBtn.jsx
+++ src/main/frontend/src/components/SelectProjectBtn.jsx
@@ -11,26 +11,28 @@ const ProjectTitle = styled.p`
     font-family: var(--eng-font);
     font-size: 80px;
     font-style: normal;
-    font-weight: 500;
-    line-height: 80px; /* 100% */
+    font-weight: bold;
+    line-height: 80px;
     letter-spacing: 9.6px;
         grid-area: title;
 
 `;
 
-const ProjectAdd=styled.p`
+const ProjectAdd = styled.p`
 color: #323232;
 font-family:  var(--eng-font);
 font-size: 16px;
 font-style: normal;
 font-weight: 500;
+line-height: 50px;
 letter-spacing: 1.92px;
     grid-area:subtitle;
 
 `
 
-const BtnBox=styled.div`
+const BtnBox = styled.div`
 border-radius: 2px;
+padding : 10px 34px;
 background: #323232;
     grid-area: button;
     
@@ -42,8 +44,7 @@ color: #FFF;
 font-family: var(--eng-font);
 font-size: 40px;
 font-style: normal;
-font-weight: 500;
-line-height: 90px; /* 225% */
+font-weight: 700;
 letter-spacing: 4.8px;
 text-decoration-line: underline;
 text-decoration-style: solid;
@@ -52,7 +53,9 @@ text-decoration-thickness: auto;
 text-underline-offset: auto;
 text-underline-position: from-font;
 text-align: center;
-    grid-area: button;
+grid-area: button;
+line-height: 60px;
+
 
 `
 
@@ -63,13 +66,14 @@ font-family: var(--eng-font);
 font-size: 80px;
 font-style: normal;
 font-weight: 500;
-line-height: 80px; /* 100% */
+
 letter-spacing: 9.6px;
 padding : 30px 27px 30px 27px;
     
 
     &:hover {
       background-color: var(--user-mainHome);
+      transition: 0.4s cubic-bezier(0.66, 0.19, 0.76, 1.13);
     } 
     
      &:hover ${ProjectTitle} {
@@ -87,26 +91,27 @@ background: rgba(255, 255, 255, 0.95);
     }   
     &:hover ${BtnText} {
 color: var(--user-mainHome, #282828);
+font-weight: bold;
     } 
 
 /* Grid 설정 */
     display : grid;
     grid-template:
 
-        "title title title ..." 160px
+        "title title title ..." 150px
         "subtitle ... ... ..." 30px
-        "... .... button ..."; 69px / 104px 226px 226px 1fr 
+        "... .... button ..."; 65px / 110px 226px 1fr  226px
 
 `;
 
 
 function SelectProjectBtn() {
     return (
-        <SelectDiv >
+        <SelectDiv>
             <ProjectTitle>PROJECT SELECT</ProjectTitle>
             <ProjectAdd>프로젝트 등록</ProjectAdd>
             <BtnBox>
-            <BtnText>CLICK</BtnText>
+                <BtnText>CLICK</BtnText>
             </BtnBox>
         </SelectDiv>
     );
