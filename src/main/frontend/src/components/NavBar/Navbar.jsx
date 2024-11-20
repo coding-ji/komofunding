64cbb587b52a3625963b4066affc31a188a560b7
kosmo <vyrjsdn5038@naver.com>
2024-11-20 10:10:27 +0900

    네비바 수정 컴포넌트

--- src/main/frontend/src/components/NavBar/Navbar.jsx
+++ src/main/frontend/src/components/NavBar/Navbar.jsx
@@ -2,19 +2,20 @@ import React, { useState, useRef, useEffect } from 'react';
 import styles from './Navbar.module.css';
 import { motion, AnimatePresence } from 'framer-motion';
 
-const Navbar = () => {
-  const categories = [
-    { name: 'clothes', content: '여기에 clothes 페이지' },
-    { name: 'pet', content: 'Pet 페이지' },
-    { name: 'accessory', content: 'Accessory 페이지' },
-    { name: 'cosmetics', content: 'Cosmetics 페이지' },
-    { name: 'home deco', content: 'Home Deco 페이지.' },
-    { name: 'travel', content: 'Travel 페이지' },
-    { name: 'food', content: 'Food 페이지' },
-    { name: 'book', content: 'Book 페이지' },
-    { name: 'etc', content: 'Etc 페이지' },
-  ];
 
+const categories = [
+  { name: 'clothes', content: '여기에 clothes 페이지' },
+  { name: 'pet', content: 'Pet 페이지' },
+  { name: 'accessory', content: 'Accessory 페이지' },
+  { name: 'cosmetics', content: 'Cosmetics 페이지' },
+  { name: 'home deco', content: 'Home Deco 페이지.' },
+  { name: 'travel', content: 'Travel 페이지' },
+  { name: 'food', content: 'Food 페이지' },
+  { name: 'book', content: 'Book 페이지' },
+  { name: 'etc', content: 'Etc 페이지' },
+];
+
+const Navbar = () => {
   const [activeCategory, setActiveCategory] = useState(categories[0]);
   const [underlineProps, setUnderlineProps] = useState({ width: 0, left: 0 });
   const navbarRef = useRef(null);
@@ -33,7 +34,7 @@ const Navbar = () => {
       const { offsetLeft, offsetWidth } = activeItem;
       setUnderlineProps({ left: offsetLeft, width: offsetWidth });
     }
-  }, [activeCategory, categories]);
+  }, [activeCategory]);
 
   return (
     <div>
@@ -54,13 +55,15 @@ const Navbar = () => {
         </div>
 
         <AnimatePresence>
-          <motion.div
-            key={activeCategory.name}
-            className={styles.underline}
-            style={{ width: underlineProps.width, left: underlineProps.left }}
-            layoutId="underline"
-            transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-          />
+          {underlineProps.width > 0 && (
+            <motion.div
+              key={activeCategory.name}
+              className={styles.underline}
+              style={{ width: underlineProps.width, left: underlineProps.left }}
+              layoutId="underline"
+              transition={{ type: 'spring', stiffness: 300, damping: 30 }}
+            />
+          )}
         </AnimatePresence>
       </div>
 
