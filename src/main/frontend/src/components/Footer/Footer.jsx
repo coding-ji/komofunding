2ece1f160a4b829d69e5d74427611648b695638f
poqazpoqaz <poqaz789@gmail.com>
2024-11-21 13:48:26 +0900

    comment 수정 반영

--- src/main/frontend/src/components/Footer/Footer.jsx
+++ src/main/frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,6 @@ const Footer = () => {
 
     return (
         <div className={styles.container}>
-            <div className={styles.wrapper1}>
                 <div className={styles.menu1}>
                     <div className={{ fontSize: "20px", fontWeight: "bold" }}>이용 안내</div>
                     <ul className={{ fontSize: "15px"}}>
@@ -27,27 +26,21 @@ const Footer = () => {
                 </div>
                 <div className={styles.text}>
                     <div>
-                        <p>코모(주) I 대표이사 코스모 I 사업자 등록 번호 I xxx-xx-xxxxx I 통신판매업 신고번호 2024-서울가산-xxxx호 I 서울 금천</p>
-                        <p>구 가산디지털 2로 101 한라원앤원타워 B동 3층 306호 I 메일주소 kosmo147@kosmo.com</p>
+                        코모(주) I 대표이사 코스모 I 사업자 등록 번호 I xxx-xx-xxxxx I 통신판매업 신고번호 2024-서울가산-xxxx호 I 서울 금천구 가산디지털 2로 101 한라원앤원타워 B동 3층 306호 I 메일주소 kosmo147@kosmo.com
                     </div>
                     <div>
-                        <p>일부 상품의 경우, KOMO FUNDING은 통신판매중개자로서 거래 책임은 판매자에게 있습니다. 사이트 내 정보의 무단 복제,</p>
-                        <p>전송, 배포, 크롤링, 스크래핑 등은 관련 법령에 의해 금지됩니다.</p>
+                        일부 상품의 경우, KOMO FUNDING은 통신판매중개자로서 거래 책임은 판매자에게 있습니다. 사이트 내 정보의 무단 복제, 전송, 배포, 크롤링, 스크래핑 등은 관련 법령에 의해 금지됩니다.
                     </div>
                     <div>
-                        <p>KOMO FUNDING은 플랫폼 제공자로서 프로젝트의 당사자가 아니며, 직접적인 통신판매를 진행하지 않습니다. 프로젝트의 완</p>
-                        <p>수와 선물 제공에 대한 책임은 창작자에게 있으며, 후원자와의 법적 분쟁 책임 또한 창작자에게 있습니다.</p>
+                        KOMO FUNDING은 플랫폼 제공자로서 프로젝트의 당사자가 아니며, 직접적인 통신판매를 진행하지 않습니다. 프로젝트의 완수와 선물 제공에 대한 책임은 창작자에게 있으며, 후원자와의 법적 분쟁 책임 또한 창작자에게 있습니다.
                     </div>
                 </div>
-            </div>
-            <div className={styles.wrapper2}>
                 <div className={styles.logo}>
                     <hr className={styles.logohr1}/>
                     <div className={styles.logohead}>KOMO FUNDING</div>
                     <hr className={styles.logohr2}/>
                 </div>
                 <div className={styles.logocopylight}>© 2024 KOMO FUNDING.All rights reserved</div>
-            </div>
         </div>
     )
 }
