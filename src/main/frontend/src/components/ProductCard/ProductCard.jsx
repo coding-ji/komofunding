7b2092407cdbde53ed5ee72ef9deb486f41567e1
KOSMO <chlek95@naver.com>
2024-11-21 15:58:58 +0900

    ProductCard 컴포넌트

--- src/main/frontend/src/components/ProductCard/ProductCard.jsx
+++ src/main/frontend/src/components/ProductCard/ProductCard.jsx
@@ -5,7 +5,7 @@ import ProductDescription from "./ProductDescription";
 
 const CardContainer = styled.div`
   display: grid; /* 그리드 레이아웃 설정 */
-  grid-template-rows: 6fr 1fr 1fr; /* 비율: 이미지 3, 제목 1, 설명 1 */
+  grid-template-rows: 5fr 0.5fr 1fr; /* 비율: 이미지 3, 제목 1, 설명 1 */
   grid-template-areas:
     "image"
     "title"
@@ -15,7 +15,7 @@ const CardContainer = styled.div`
   border-radius: 8px;
   overflow: hidden;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  background: #fff;
+
 `;
 
 // 이미지 영역
@@ -23,7 +23,6 @@ const StyledProductImg = styled(ProductImg)`
   grid-area: image; /* 그리드 영역 이름과 연결 */
   width: 100%;
   height: 100%;
-
   background-position: center;
 `;
 
@@ -33,13 +32,17 @@ const StyledProductTitle = styled(ProductTitle)`
   text-align: center;
   font-size: 1.2rem;
   font-weight: bold;
+   width: 100%; /* 부모 컨테이너에 맞게 너비 설정 */
+  padding: 0 10px; 
 `;
 
 // 설명 영역
 const StyledProductDescription = styled(ProductDescription)`
   grid-area: description; /* 그리드 영역 이름과 연결 */
   padding: 0 10px; /* 양옆 여백 추가 */
-  text-align: left; /* 텍스트를 왼쪽 정렬 */
+    text-align: center;
+  
+
   font-size: 0.9rem;
   color: #666;
 `;
@@ -49,7 +52,7 @@ function ProductCard({ src, title, description }) {
     <CardContainer>
       <StyledProductImg src={src} />
       <StyledProductTitle title={title} />
-      <StyledProductDescription description={"description"} />
+      <StyledProductDescription description={description} />
     </CardContainer>
   );
 }
--- src/main/frontend/src/components/ProductCard/ProductTitle.jsx
+++ src/main/frontend/src/components/ProductCard/ProductTitle.jsx
@@ -5,8 +5,8 @@ const StyledTitle = styled.div`
   font-size: 2rem;
   font-weight : bold;
   margin: 4px 0;
-
-  text-align: center;
+  padding: 0 8px; /* 양쪽 여백 */
+  text-align: left;
   
   font-family : var(--eng-bold);
   
