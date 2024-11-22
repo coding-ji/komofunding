dfc1a072e4d86722b229982f7251734fe24f6f1b
KOSMO <chlek95@naver.com>
2024-11-22 12:15:24 +0900

    헤더

---
+++ src/main/frontend/src/components/ProductCard1/ProductTitle.jsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import '../../index.css';
+
+const StyledTitle = styled.div`
+  font-size: 2.2rem;
+  font-weight : bold;
+  margin: 4px 0;
+  padding: 0 8px; /* 양쪽 여백 */
+  text-align: left;  
+  color: ${(props) => {
+    if (props.fontFamily === "var(--eng-font)") {
+      return "var(--Titletext-blackcolor)";
+    } else if (props.fontFamily === "var(--kr-font)") {
+      return "var(--darkgreen-color)";
+    }
+    return "var(--Titletext-blackcolor)";
+  }};
+
+`;
+
+function ProductTitle({ title, fontFamily }) {  
+  return <StyledTitle fontFamily={fontFamily}>{title}</StyledTitle>;
+}
+
+export default ProductTitle;
