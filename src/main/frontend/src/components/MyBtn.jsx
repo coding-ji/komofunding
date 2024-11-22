be79e909bc25cbe50472d49ea3138422be7fd939
수현 <tngus1633@gmail.com>
2024-11-22 18:36:41 +0900

    product 컴포넌트

---
+++ src/main/frontend/src/components/MyBtn.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import '../index.css';
+
+const StyledBtn = styled(motion.button)`
+  width: 100%;
+  height: 100%;
+  background-color: var(--darkblue-color);
+  color: white;
+  text-align: center;
+  border-radius: 2px;
+  padding: 10px 112px;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+  font-family: var(--eng-bold);
+  margin: 4px 0;
+  font-size: 2rem;
+  border: 2px solid var(--darkblue-color); /* 기본 보더라인 설정 */
+  border-radius : 2px;
+  
+
+
+  /* 호버 시 전환될 스타일 */
+  transition: all 0.2s ease; /* 부드러운 전환 효과 */
+`;
+
+function MyBtn({ text }) {
+  return (
+    <StyledBtn
+      whileHover={{
+        backgroundColor: "white",  // 호버 시 배경색을 흰색으로 변경
+        color: "var(--darkblue-color)",  // 호버 시 글자색을 다크블루로 변경
+        border: "2px solid var(--darkblue-color)", // 호버 시 보더라인 색을 다크블루로 변경
+      }}
+    >
+      {text}
+    </StyledBtn>
+  );
+}
+
+export default MyBtn;
---
+++ src/main/frontend/src/components/MyContainers.jsx
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import '../index.css';
+import MyContainer from "./MyContainer";
+
+// 그리드 레이아웃을 위한 스타일링
+const StyledContainers = styled(motion.div)`
+
+  display: grid;
+  grid-template-columns: repeat(3, auto);
+  gap: 25px; /* 요소 간 간격 */
+  margin: 0 auto; /* 컨테이너 가운데 정렬 */
+
+  @media (max-width: 920px) {
+    grid-template-columns: repeat(2, auto); /* 열 개수 자동 조정 */
+  }
+
+  @media (max-width: 700px) {
+    grid-template-columns: repeat(1, auto); /* 1열 유지 */
+    padding: 0 10px; /* 여백 최소화 */
+  }
+`;
+
+
+// 그리드의 부모 컨테이너 애니메이션 설정
+const containerVariants = {
+  initial: { opacity: 0 },
+  animate: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.2, // 자식 요소가 등장하기 전 대기 시간
+      staggerChildren: 0.2, // 자식 요소들 간의 등장 간격 
+    },
+  },
+};
+
+// 자식 요소에 대한 애니메이션 설정
+const itemVariants = {
+  initial: { opacity: 0, y: -100 }, // 시작 시 opacity 0, 아래에서 올라옴
+  animate: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeOut" } },
+};
+
+function MyContainers() {
+  return (
+    <StyledContainers
+      variants={containerVariants}  // 부모 요소에 애니메이션 variants 추가
+      initial="initial"
+      animate="animate"
+    >
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+      <motion.div variants={itemVariants}>
+        <MyContainer title="포실포실하덕" description="얄루얄루" text="DELETE" />
+      </motion.div>
+    </StyledContainers>
+  );
+}
+
+export default MyContainers;
