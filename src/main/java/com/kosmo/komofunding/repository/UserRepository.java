fa60478803bace521e581e7bd3f4171510c27253
coding-ji <eunjikwon0530@hotmail.com>
2024-11-25 10:56:48 +0900

    User 엔티티 수정

--- src/main/java/com/kosmo/komofunding/repository/UserRepository.java
+++ src/main/java/com/kosmo/komofunding/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.kosmo.komofunding.repository;
 
-import org.apache.catalina.User;
+import com.kosmo.komofunding.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
-    boolean existsById(String uid);
-    boolean existsByEmail(String email);
 }
