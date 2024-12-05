c43f2476a2af223af5132ebbb543eb06e32ee4f2
KOSMO <chlek95@naver.com>
2024-12-05 19:47:23 +0900

    문의하기

--- src/main/frontend/src/page/MyPage/writeQnA/WriteQnA.jsx
+++ src/main/frontend/src/page/MyPage/writeQnA/WriteQnA.jsx
@@ -1,45 +1,86 @@
+import React, { useState, useRef } from "react";
 import Input from "../../../components/input";
 import TitleText from "../../../components/TitleText";
-import { useState } from "react";
 import styles from "./WriteQnA.module.css";
 import Editor from "../../../components/EditorItem/EditorItem";
-// import Footer from '../../components/Footer/Footer'
+import PopupInquiry from "./PopupInquiry"
+import { Btn, WhiteBtn } from "../../../components/MyBtn";
+import { useNavigate } from "react-router-dom";
 
-// 문의하기 글쓰기
 function WriteQnA() {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [editorContent, setEditorContent] = useState(""); // 에디터 내용
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const quillRef = useRef(null); // Quill 인스턴스 참조
 
+  const navigate = useNavigate();
 
+  const handleSave = () => {
+    console.log(title , editorContent );
+    setIsPopupOpen(true);
+  };
 
-    return (
-        <>
+  const handleCancel = () => {
+    navigate("/myqna");
+    setTitle("");
+    setEditorContent("");
+  };
 
-        <div className={styles.mainGrid}>
-            {/* 상단 섹션 */}
-            <div className={styles.topSection}>
-                <TitleText title="문의하기" />
-            </div>
+  const handlePopupClose = () => {
+    setIsPopupOpen(false);
+  };
 
-            {/* 중단 섹션 */}
-            <div className={styles.middleSection}>
-                <p className={styles.subtitle}>제목</p>
-                <Input
-                    className={styles.input}
-                    type="text"
-                    placeholder="문의 제목을 입력하세요"
-                    value={title}
-                    onChange={(e) => setTitle(e.target.value)}
-                />
-                <p className={styles.subtitle}>글 내용</p>
-                      <Editor content={content} setContent={setContent} />
-              
-            </div>
+  return (
+    <>
+      <div className={styles.mainGrid}>
+        <div className={styles.topSection}>
+          <TitleText title="문의하기" />
         </div>
 
-        {/* <Footer/> */}
-        </>
-    );
+        <div className={styles.middleSection}>
+          <p className={styles.subtitle}>제목</p>
+          <Input
+            className={styles.input}
+            type="text"
+            placeholder="문의 제목을 입력하세요"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <p className={styles.subtitle}>글 내용</p>
+          <Editor
+            editorContent={editorContent}
+            setEditorContent={setEditorContent}
+            quillRef={quillRef}
+          />
+        </div>
+
+        <div className={styles.buttonContainer}>
+
+          <Btn text="확인" onClick={handleSave} 
+          width="100px" height="40px" fontSize="1.1rem" padding="2px 1rem"
+           />
+          <WhiteBtn text="취소" onClick={handleCancel}
+           width="100px" height="40px" fontSize="1.1rem" padding="2px 1rem"
+            />
+
+        </div>
+      </div>
+
+      {isPopupOpen && (
+        <PopupInquiry
+          message={
+            <>
+              문의 글이 등록되었습니다.
+              <br />
+              답변은 나의 문의 내역에서 확인하세요.
+            </>
+          }
+          onClose={handlePopupClose}
+          navigateTo="/myqna"
+        />
+      )}
+    </>
+  );
 }
 
 export default WriteQnA;
