3a5343962c4278c174625ec7f6c40e0ba6193e1a
suhyeon <tngus1633@gmail.com>
2024-12-05 16:43:37 +0900

    나의 후원 + 프로젝트 등록 수정

--- src/main/frontend/src/components/ProductList.jsx
+++ src/main/frontend/src/components/ProductList.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ProductItem from "./ProductItem"; // ProductItem 컴포넌트 임포트
 import styled from "styled-components";
+import '../index.css';
+
 
 const ProductDetails = styled.div`
   margin-top: 20px;
   padding: 10px;
   border: 1px solid #ddd;
   border-radius: 8px;
+  font-family : var(--kr-font);
+  font-size : 1rem;
+
 `;
 
 function ProductList({ products }) {
