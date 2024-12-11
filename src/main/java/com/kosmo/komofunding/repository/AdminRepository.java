632bfd79b8f11d6aa3399516ae25ff7016f2d922
kosmo <vyrjsdn5038@naver.com>
2024-12-11 16:29:54 +0900

    수정

--- src/main/java/com/kosmo/komofunding/repository/AdminRepository.java
+++ src/main/java/com/kosmo/komofunding/repository/AdminRepository.java
@@ -9,21 +9,21 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, String> {
-    //관리자 UID로 조회
 
-    Admin findByAdminId(String adminId);
+    // 관리자 UID로 조회
+    Admin findByAdminId(String adminId); // 수정됨
+
     // 관리자 이메일로 조회
     Admin findByAdminEmail(String adminEmail);
 
     // 관리자 별명 검색 (포함 검색)
     List<Admin> findByAdminNicknameContaining(String adminNickname);
 
-    // 공지사항/이벤트 글 ID가 포함된 관리자 검색
-    List<Admin> findByNoticeIdListContaining(String noticeId);
-
     // 특정 가입 날짜 범위의 관리자 검색
     List<Admin> findByAdminJoinDateBetween(LocalDateTime startDate, LocalDateTime endDate);
 
 }
+
